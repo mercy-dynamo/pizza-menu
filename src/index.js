da153b4995b029e2f15ed3b6c61b126a4c16ec43
Samarth Patel <samarthnimeshkumarpatel@gmail.com>
2025-02-08 13:39:44 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -67,27 +67,38 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price="10"
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price="12"
+      />
     </main>
+  );
+}
+function Pizza(props) {
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name} />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+    </div>
   );
 }
 function Footer() {
   return (
-    <footer className="footer">{new Date().toLocaleTimeString()} We're Currently open!</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We're Currently open!
+    </footer>
   );
 }
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
